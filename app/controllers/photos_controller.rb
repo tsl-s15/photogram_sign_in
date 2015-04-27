0faaa4c7d923e42b0860a9eb0ec447fe12cba38d
Arjun Venkataswamy <avenkat2@gmail.com>
2015-04-27 11:24:40 -0500

    adds golden seven

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,4 +2,40 @@ class PhotosController < ApplicationController
   def show
     @photo = Photo.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    @photo = Photo.new
+    @photo.source = params[:source]
+    @photo.caption = params[:caption]
+    @photo.save
+
+    redirect_to "/photos/#{ @photo.id }"
+  end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+
+    redirect_to "/photos"
+  end
+
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+    @photo.source = params[:source]
+    @photo.caption = params[:caption]
+    @photo.save
+
+    redirect_to "/photos/#{ @photo.id }"
+  end
+
+  def index
+    @photos = Photo.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+
+  # Routes to CREATE photos
+  get("/photos/new",       { :controller => "photos", :action => "new" })
+  get("/create_photo",    { :controller => "photos", :action => "create" })
+
   # Routes to READ photos
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+  get("/photos", { :controller => "photos", :action => "index" })
+
+  # Routes to UPDATE photos
+  get("/photos/:id/edit",       { :controller => "photos", :action => "edit" })
+  get("/update_photo/:id", { :controller => "photos", :action => "update"})
+
+  # Routes to DESTROY photos
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
 end
