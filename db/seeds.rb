e9e45038ac3689286bc502e2ebed1dfe38145c9e
Arjun Venkataswamy <avenkat2@gmail.com>
2015-05-05 23:12:40 -0500

    updates readme for associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,32 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
+User.destroy_all
+user_info = [
+  {
+    name: "Brianna Woods",
+    email: "brianna.woods19@example.com",
+    image_url: "https://randomuser.me/api/portraits/med/women/69.jpg"
+  },
+  {
+    name: "Randy Baker",
+    email: "randy.baker89@example.com",
+    image_url: "https://randomuser.me/api/portraits/med/men/28.jpg"
+  },
+  {
+    name: "Isaac Gonzalez",
+    email: "isaac.gonzalez65@example.com",
+    image_url: "https://randomuser.me/api/portraits/med/men/10.jpg"
+  }
+]
+
+user_info.each do |user|
+  User.create!(user)
+end
+puts "There are now #{User.count} users in the database."
+
+Photo.destroy_all
 photo_info = [
   {
     :source => "http://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Lake_Bondhus_Norway_2862.jpg/1280px-Lake_Bondhus_Norway_2862.jpg",
@@ -41,7 +67,24 @@ photo_info.each do |photo_hash|
   p = Photo.new
   p.source = photo_hash[:source]
   p.caption = photo_hash[:caption]
+  rand_num = rand(User.count)
+  p.user_id = User.offset(rand_num).first.id
   p.save
 end
-
 puts "There are now #{Photo.count} photos in the database."
+
+
+# 20.times do
+#   c = Comment.new
+#   rand_num = rand(User.count)
+#   c.user_id = User.offset(rand_num).first.id
+#   rand_num = rand(Photo.count)
+#   c.photo_id = Photo.offset(rand_num).first.id
+#   c.contents = Faker::Hacker.say_something_smart
+#   c.save
+# end
+
+
+
+
+
